c40edeb4b0b1f2a0a2814cc772ca3498a7afb3c6
Pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2022-08-04 13:16:01 +0930

    4

--- Referee.cpp
+++ Referee.cpp
@@ -6,8 +6,10 @@
 using namespace std;
 Referee::Referee(){}
 char Referee::refGame(Human player1, Computer player2){
-    if (player1.makeMove() == 'P' && player2.makeMove() == 'R'){return 'W';}
-    else if (player1.makeMove() == 'S' && player2.makeMove() == 'R'){return 'L';}
-    else if (player1.makeMove() == 'R' && player2.makeMove() == 'R'){return 'T';}
+    char player1move = player1.getmove();
+    char player2move = player2.getmove();
+    if (player1move == 'P' && player2move == 'R'){return 'W';}
+    else if (player1move == 'S' && player2move == 'R'){return 'L';}
+    else if (player1move == 'R' && player2move == 'R'){return 'T';}
     return 'l';
 }
