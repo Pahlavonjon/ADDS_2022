b3bae816bebd75072999d5f84de1f6643a7231f7
Pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2022-08-18 13:35:28 +0930

    edited

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 int main(){
     Library* Adelaide_University_Library = new Library();
-    Book* Harry_Potter = new Book(1001);
-    Book* Rald_Dalph = new Book(1002);
+    Book* Harry_Potter = new Book(1001); // sub type polymorphism
+    Book* Rald_Dalph = new Book(1002); // sub type polymorphism
     Adelaide_University_Library->addDocument(Harry_Potter);
     Adelaide_University_Library->addDocument(Rald_Dalph);
     cout << "\n "<<Adelaide_University_Library->hasDocument(Harry_Potter)<< "\n\n";
